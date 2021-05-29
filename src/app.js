0d46f17fc28941bcab7f03d636f33bcc88c356e3
cristiangutie <cristian.gutie@gmail.com>
2021-05-29 14:45:36 +0000

    solution using map method

--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".4geeks", ".cl", ".com", ".edu", ".us"];
+
+console.log(
+  pronoun.flatMap(b =>
+    adj.map(c => noun.map(d => domain.map(a => b + c + d + a)))
+  )
+);
