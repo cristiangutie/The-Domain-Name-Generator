086eae00e9f0bb0344b204d1b37ada28246606fc
cristiangutie <cristian.gutie@gmail.com>
2021-05-29 14:53:09 +0000

    solution using flatMap

--- src/app.js
+++ src/app.js
@@ -16,6 +16,6 @@ let domain = [".4geeks", ".cl", ".com", ".edu", ".us"];
 
 console.log(
   pronoun.flatMap(b =>
-    adj.map(c => noun.map(d => domain.map(a => b + c + d + a)))
+    adj.flatMap(c => noun.flatMap(d => domain.flatMap(a => b + c + d + a)))
   )
 );
