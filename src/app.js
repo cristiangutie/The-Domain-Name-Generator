4b98a5a02826bc9249d4a96e9db7c8d49b6d88ba
cristiangutie <cristian.gutie@gmail.com>
2021-05-29 16:46:56 +0000

    minor fix for explanation purpuses

--- src/app.js
+++ src/app.js
@@ -9,13 +9,14 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let domain = [".4geeks", ".cl", ".com", ".edu", ".us"];
 
 console.log(
-  pronoun.flatMap(b =>
-    adj.flatMap(c => noun.flatMap(d => domain.flatMap(a => b + c + d + a)))
+  pronoun.flatMap(a =>
+    adj.flatMap(b => noun.flatMap(c => domain.flatMap(d => a + b + c + d)))
   )
 );
